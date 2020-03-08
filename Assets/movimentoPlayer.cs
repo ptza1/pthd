f01aa74b3df69dcc8bca47c03f1d25200aaf231f
Toatreezel <toatreezel@gmail.com>
2020-03-08 17:39:43 -0300

    com cenario basico e textura basica
    lanterna n ta funcionando btw

--- Assets/movimentoPlayer.cs
+++ Assets/movimentoPlayer.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class movimentoPlayer : MonoBehaviour
 {
-    public CharacterController controller;
+    CharacterController controller;
 
     public float speed = 12f;
 
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Verical");
+        float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
